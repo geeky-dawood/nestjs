0caa9caed5c9301ed0fb2822d802c509dcf0943f
Muhammad Dawood <dawood.zafar@geekybugs.com>
2024-10-28 19:46:10 +0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ var cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  app.use(cors({ origin: '*' }));
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT || 3001);
